b963b22bcd0f9a0d71bf0447b12b434fd195295e
Linny <github@quackers.slmail.me>
2022-04-21 11:50:51 +1200

    stole some code for client/server, need to clean up

--- Classes/Client.py
+++ Classes/Client.py
@@ -10,3 +10,31 @@ def send(message):
     finally:
         print("Closing socket") # closing the socket
         tcpSocket.close()
+
+def recieve():
+    # take the server name and port name
+  
+    host = 'local host'
+    port = 5000
+    
+    # create a socket at client side
+    # using TCP / IP protocol
+    s = socket.socket(socket.AF_INET,
+                    socket.SOCK_STREAM)
+    
+    # connect it to server and port
+    # number on local computer.
+    s.connect(('127.0.0.1', port))
+    
+    # receive message string from
+    # server, at a time 1024 B
+    msg = s.recv(1024)
+    
+    # repeat as long as message
+    # string are not empty
+    while msg:
+        print('Received:' + msg.decode())
+        msg = s.recv(1024)
+    
+    # disconnect the client
+    s.close()
